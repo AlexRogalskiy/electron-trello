3e2e29b19bb90038b6944c4402593743d18fb55e
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-10 05:15:26 +0300

    Init repo

---
+++ menu.js
@@ -0,0 +1,245 @@
+'use strict';
+const os = require('os');
+const path = require('path');
+const {app, shell, dialog, BrowserWindow, Menu} = require('electron');
+
+const appName = app.getName();
+
+function activate(command) {
+  const appWindow = BrowserWindow.getAllWindows()[0];
+  // Extra measure in order to be shown
+  appWindow.show();
+  appWindow.webContents.send(command);
+}
+
+const helpSubmenu = [{
+  label: `${appName} Website`,
+  click() {
+    shell.openExternal('https://github.com/1000ch/whale');
+  }
+}, {
+  label: 'Report an Issue...',
+  click() {
+    const body = `
+<!-- Please succinctly describe your issue and steps to reproduce it. -->
+-
+${app.getName()} ${app.getVersion()}
+Electron ${process.versions.electron}
+${process.platform} ${process.arch} ${os.release()}`;
+
+    shell.openExternal(`https://github.com/1000ch/whale/issues/new?body=${encodeURIComponent(body)}`);
+  }
+}];
+
+if (process.platform !== 'darwin') {
+  helpSubmenu.push({
+    type: 'separator'
+  }, {
+    role: 'about',
+    click() {
+      dialog.showMessageBox({
+        title: `About ${appName}`,
+        message: `${appName} ${app.getVersion()}`,
+        detail: 'Created by Shogo Sensui',
+        icon: path.join(__dirname, 'static/Icon.png'),
+        buttons: []
+      });
+    }
+  });
+}
+
+const darwinTpl = [{
+  label: appName,
+  submenu: [{
+    role: 'about'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'services',
+    submenu: []
+  }, {
+    type: 'separator'
+  }, {
+    role: 'hide'
+  }, {
+    role: 'hideothers'
+  }, {
+    role: 'unhide'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'quit'
+  }]
+}, {
+  label: 'Edit',
+  submenu: [{
+    role: 'undo'
+  }, {
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'cut'
+  }, {
+    role: 'copy'
+  }, {
+    role: 'paste'
+  }, {
+    role: 'pasteandmatchstyle'
+  }, {
+    role: 'delete'
+  }, {
+    role: 'selectall'
+  }]
+}, {
+  label: 'View',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.reload();
+      }
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Make Text Larger',
+    accelerator: 'CmdOrCtrl+Plus',
+    click() {
+      activate('zoom-in');
+    }
+  }, {
+    label: 'Make Text Smaller',
+    accelerator: 'CmdOrCtrl+-',
+    click() {
+      activate('zoom-out');
+    }
+  }, {
+    label: 'Reset Zoom Level',
+    accelerator: 'CmdOrCtrl+0',
+    click() {
+      activate('zoom-reset');
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Toggle Full Screen',
+    accelerator: 'Ctrl+Command+F',
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        focusedWindow.send('window:fullscreen', {state: focusedWindow.isFullScreen()});
+      }
+    }
+  }, {
+    label: 'Toggle Developer Tools',
+    accelerator: 'Alt+Command+I',
+    click: (item, focusedWindow) => {
+      focusedWindow.toggleDevTools();
+    }
+  }]
+}, {
+  role: 'window',
+  submenu: [{
+    role: 'minimize'
+  }, {
+    role: 'close'
+  }, {
+    type: 'separator'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'front'
+  }, {
+    role: 'togglefullscreen'
+  }]
+}, {
+  role: 'help',
+  submenu: helpSubmenu
+}];
+
+const otherTpl = [{
+  label: 'File',
+  submenu: [{
+    role: 'quit'
+  }]
+}, {
+  label: 'Edit',
+  submenu: [{
+    role: 'undo'
+  }, {
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'cut'
+  }, {
+    role: 'copy'
+  }, {
+    role: 'paste'
+  }, {
+    role: 'pasteandmatchstyle'
+  }, {
+    role: 'delete'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'selectall'
+  }]
+}, {
+  label: 'View',
+  submenu: [{
+    label: 'Reload',
+    accelerator: 'CmdOrCtrl+R',
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.reload();
+      }
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Make Text Larger',
+    accelerator: 'CmdOrCtrl+Plus',
+    click() {
+      activate('zoom-in');
+    }
+  }, {
+    label: 'Make Text Smaller',
+    accelerator: 'CmdOrCtrl+-',
+    click() {
+      activate('zoom-out');
+    }
+  }, {
+    label: 'Reset Zoom Level',
+    accelerator: 'CmdOrCtrl+0',
+    click() {
+      activate('zoom-reset');
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Toggle Full Screen',
+    accelerator: 'F11',
+    click: (item, focusedWindow) => {
+      if (focusedWindow) {
+        focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+        focusedWindow.send('window:fullscreen', {state: focusedWindow.isFullScreen()});
+      }
+    }
+  }, {
+    label: 'Toggle Developer Tools',
+    accelerator: 'Ctrl+Shift+I',
+    click: (item, focusedWindow) => {
+      focusedWindow.toggleDevTools();
+    }
+  }]
+}, {
+  role: 'help',
+  submenu: helpSubmenu
+}];
+
+const tpl = process.platform === 'darwin' ? darwinTpl : otherTpl;
+
+module.exports = Menu.buildFromTemplate(tpl);
---
+++ readme.md
@@ -0,0 +1,43 @@
+# Whale
+
+Unofficial [Trello](https://trello.com/) app
+
+[![Build Status](https://travis-ci.org/1000ch/whale.svg?branch=master)](https://travis-ci.org/1000ch/whale)
+[![Dependency Status](https://david-dm.org/1000ch/whale.svg)](https://david-dm.org/1000ch/whale)
+[![devDependency Status](https://david-dm.org/1000ch/whale/dev-status.svg)](https://david-dm.org/1000ch/whale?type=dev)
+
+![Whale demo](demo.png)
+
+## Install
+
+macOS 10.9+ & Linux are supported.
+
+### macOS
+
+[Download](https://github.com/1000ch/whale/releases) and extract `.zip`, and move `Whale.app` to `/Applications`.
+
+Alternatively, if you use [Homebrew](http://brew.sh/), you can install via:
+
+```
+brew cask install whale
+```
+
+### Windows
+
+[Download](https://github.com/1000ch/whale/releases) and extract `.zip`, and move `Whale.exe` to some location.
+
+### Linux
+
+[Download](https://github.com/1000ch/whale/releases) and extract `.zip`, and move `Whale` to some location.
+
+## Keyboard shortcuts
+
+Description                | Keys
+-------------------------- | --------------------------
+Reset Zoom Level           | <kbd>Cmd/Ctrl</kbd> <kbd>0</kbd>
+Make Text Smaller          | <kbd>Cmd/Ctrl</kbd> <kbd>-</kbd>
+Make Text Larger           | <kbd>Cmd/Ctrl</kbd> <kbd>Shift</kbd> <kbd>=</kbd>
+
+## License
+
+[MIT](https://1000ch.mit-license.org) © [Shogo Sensui](https://github.com/1000ch)
